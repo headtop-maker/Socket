65104c5c5f2c2127b04b632f3f1dc451887d0758
headtop <headtop@mail.ru>
2022-08-24 22:09:39 +0300

    debug: странный тест

--- android/app/src/main/java/com/socket/TestClass.kt
+++ android/app/src/main/java/com/socket/TestClass.kt
@@ -1,28 +1,27 @@
-package com.socket
+// package com.socket
 
-import android.content.Intent
-import android.os.Environment
-import android.provider.DocumentsContract
-import android.util.Log
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
-import kotlin.concurrent.thread
+// import android.content.Intent
+// import android.os.Environment
+// import android.provider.DocumentsContract
+// import android.util.Log
+// import androidx.activity.result.contract.ActivityResultContracts
+// import androidx.appcompat.app.AppCompatActivity
+// import kotlin.concurrent.thread
 
-class TestClass(name:String): AppCompatActivity() {
+// class TestClass(name:String): AppCompatActivity() {
 
-    val firstProperty = "Первое свойство: $name".also(::println)
+//     val firstProperty = "Первое свойство: $name".also(::println)
 
-    init {
-        Log.d("ClassInit 1",name)
-        println("Первый блок инициализации: ${name}")
-    }
+//     init {
+//         Log.d("ClassInit 1",name)
+//         println("Первый блок инициализации: ${name}")
+//     }
 
-    val secondProperty = "Второе свойство: ${name.length}".also(::println)
+//     val secondProperty = "Второе свойство: ${name.length}".also(::println)
 
-    init {
-        Log.d("ClassInit 2",name)
-        println("Второй блок инициализации: ${name.length}")
-    }
+//     init {
+//         Log.d("ClassInit 2",name)
+//         println("Второй блок инициализации: ${name.length}")
+//     }
 
-
-}
+// }
